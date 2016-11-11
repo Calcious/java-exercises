6105c4989315aad3b30f9007e50df3b9324fdba1
Justin Armer <justin.armer@gmail.com>
2016-11-11 11:42:57 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Calcious on 11/11/16.
+ */
+//every Java project should be in a src folder
+public class HelloWorld {
+    public static void main(String[] args) {
+        //What every Java program starts with--use "psvm" to do the same thing
+        System.out.println("Hello Mammoth"); //also "sout" for shortcut
+        //like console.log in JS--Also DOUBLE QUOTES only in Java for strings
+        System.out.print("Hello world!");
+        System.out.printf("Hello %s, today is %s, and its the %dth day of the week ", //printf = print format
+                "Codeup",
+                "Friday",
+                5);
+    }
+}
+
