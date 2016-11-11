32deb4929e1b84f0305f811e005f89c148a57d34
Justin Armer <justin.armer@gmail.com>
2016-11-11 16:54:49 -0600

    Introduction to Java continued//Scanner exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
 /**
  * Created by Calcious on 11/11/16.
  */
+
+import java.util.Scanner;
+
 //every Java project should be in a src folder
 public class HelloWorld {
     /**
@@ -8,31 +11,52 @@ public class HelloWorld {
      * @param args These are the arguments passed through the terminal
      */
     public static void main(String[] args) {
-        int dayNumber = 5;
 
-        float price;
-        price = (float) 2.3; //type casting
+        Scanner sc = new Scanner(System.in);
+        int width;
+        int length;
+        String userInput = "no";
 
-        double anotherPrice = 6.5;
+//        do {
+//
+//            System.out.println("What is the length of the classroom?");
+//            int length = sc.nextInt();
+//
+//            System.out.println("What is the width of the classroom?");
+//            int width = sc.nextInt();
+//
+//            int area = length * width;
+//            System.out.println(area);
+//
+//            System.out.println("Would you like to proceed?  Yes or no?");
+//            String yes = "Yes";
+//            String no = "No";
+//
+//        } while (yes);
 
+        do {
 
-        char vowel;
-        vowel = 'e'; //single quotes are reserved for single characters only
+            System.out.print("Welcome to the room calculator");
+            System.out.print("Enter width: ");
+            width = sc.nextInt();
+            System.out.print("Enter length: ");
+            length = sc.nextInt();
 
-        boolean isFriday = true;
+            System.out.println("The room has a perimeter of " + perimeter(width, length));
+            System.out.println("The room has an area of " + area(width, length));
 
+            System.out.println("Thank you for using the room calculator.  Would you like to calculate another room?");
+            userInput = sc.next();
+        } while (userInput.equals("yes"));
+        System.out.println("Thank you for using the room calculator");
+    }
 
-        String day = "Friday";
+    public static int perimeter(int width, int height){
+        return 2 * width + 2 * height;
+    }
 
-
-        //What every Java program starts with--use "psvm" to do the same thing
-        System.out.println("Hello Mammoth"); //also "sout" for shortcut
-        //like console.log in JS--Also DOUBLE QUOTES only in Java for strings
-        System.out.print("Hello world!");
-        System.out.printf("Hello %s, today is %s, and its the %dth day of the week ", //printf = print format
-                "Codeup",
-                day,
-                dayNumber);
+    public static int area(int width, int height){
+        return width * height;
     }
 }
 
