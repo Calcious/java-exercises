35a5bcf5d7d69ea84af992019a0b84602dc08f34
Justin Armer <justin.armer@gmail.com>
2016-11-14 13:35:08 -0600

    Strings exercises

---
+++ src/Stringbasics.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Calcious on 11/14/16.
+ */
+public class Stringbasics {
+    public static void main(String[] args) {
+//        StringBuffer strEdu = new StringBuffer("We don't need no education ");
+//        strEdu.append("we don't need no thought control ");
+//        System.out.println(strEdu);
+//        String educAtion = "We don't need no education \n We don't need no thought control";
+//        System.out.println(educAtion);
+//        String stringThis = "Check this out\"!, \"s inside of \"s!";
+//        System.out.println(stringThis);
+
+//        String winDows = "In Windows, the main drive is usually C:\\ ";
+//        System.out.println(winDows);
+
+        String backSlash = "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!";
+        System.out.println(backSlash);
+    }
+}
