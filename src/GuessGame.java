317507e5c10a7fea8988dbc9ca04b2db1b6d8d93
Justin Armer <justin.armer@gmail.com>
2016-11-16 09:47:01 -0600

    Guessing game more developed//Trying to solve an issue whereby the game continues even if the guess is out of range

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.InputMismatchException;
 
 /**
  * Created by Calcious on 11/15/16.
@@ -7,23 +8,41 @@ import java.lang.Math;
 public class GuessGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int userInput;
+        int userInput = 0;
         int randomNumber = 1 + (int)(Math.random() * ((100 - 1) + 1));
 
+
         System.out.println("Please take the time to guess a number between 1 and 100...");
-        userInput = sc.nextInt();
+        exceptAtt(userInput);
+
+        do {
+            System.out.println("I'm sorry, but the number you have chosen is out of range...please select another number");
+            break;
+        } while(userInput < 1 || userInput > 100);
 
         do {
             if (userInput > randomNumber) {
                 System.out.println("LOWER");
-                userInput = sc.nextInt();
+                exceptAtt(userInput);
             } else if (userInput < randomNumber) {
                 System.out.println("HIGHER");
-                userInput = sc.nextInt();
-            } else if (userInput == randomNumber){
-                System.out.println("Congratulations--you guessed right!");
-                break;
+                exceptAtt(userInput);
             }
         } while (userInput != randomNumber);
+        System.out.println("You guessed right!!");
     }
+
+    public static int exceptAtt(int userInput){
+        Scanner sc = new Scanner(System.in);
+        try {
+            userInput = sc.nextInt();
+        } catch (InputMismatchException e){
+            System.out.println();
+        }
+        return userInput;
+    }
+
+//    public static void endGame(){
+//
+//    }
 }
